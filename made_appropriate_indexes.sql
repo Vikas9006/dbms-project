2783fc03e0e3de70b4794aacbc55e0febb6993df
AaryumanSawai <122730648+AaryumanSawai@users.noreply.github.com>
2023-05-01 08:19:05 +0530

    Add files via upload

---
+++ made_appropriate_indexes.sql
@@ -0,0 +1,29 @@
+﻿– index for transaction table -: 
+
+
+create index idx_transaction_sender_id on transaction using btree(sender_id);
+
+
+create index idx_transaction_receiver_id on transaction using btree(receiver_id);
+
+
+
+
+-index on loan table
+
+
+create index idx_loan_loan_id on loan using btree(loan_id);
+
+
+-index on loan_repayment table -: 
+
+
+create index idx_loan_repayment_repayment_id on loan_repayment using btree(repayment_id);
+
+
+
+
+-index on Account table-: 
+
+
+create index idx_Account_account_number on Account using btree(account_number);
